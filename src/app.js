0e935fbfbc1c874d49e8cfcc3ba92efd062ed968
VijayRanghar <vijayranghar@gmail.com>
2018-03-24 15:58:50 +0530

    initialize webpack setup

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        This is the demo app
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById("app"))
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname,'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, use:'babel-loader' }
+    ]
+  }
+}
